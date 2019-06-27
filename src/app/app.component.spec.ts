d3c98825e790b55b2f4c5ef6a8bcf7f73dc5f826
Toma1997 <toma.joksimovic@gmail.com>
2019-06-28 01:43:08 +0200

    Changing theme in application

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'singipool-app'`, () => {
+  it(`should have as title 'Singidunum Carpooling'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('singipool-app');
+    expect(app.title).toEqual('Singidunum Carpooling');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to singipool-app!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Singidunum Carpooling!');
   });
 });
